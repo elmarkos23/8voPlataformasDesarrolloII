67b805a81341576a4e912c2d596826722ec9a3b6
Markos Ayala <elmarkos23@gmail.com>
2021-04-19 21:03:20 -0500

    .
    .

---
+++ Semana2/LabSemana2/Pages/Index.cshtml.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+
+namespace LabSemana2.Pages
+{
+  public class IndexModel : PageModel
+  {
+    public string Time { get; set; }
+    public void OnGet()
+    {
+      Time = DateTime.Now.ToString("dd/MM/yyyy");
+    }
+  }
+}
